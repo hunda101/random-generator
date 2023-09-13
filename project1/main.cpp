e3d14a4c7659ba10a7a322d4aa00f95f73f5759e
hunda101 <47692698+hunda101@users.noreply.github.com>
2023-09-13 08:20:03 +0300

    Initial Commit

---
+++ project1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by Mark Khomenko on 13.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
