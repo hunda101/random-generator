aeccb4476cd210c7d4b6a312e1edf0928d5fb669
hunda101 <2markkhomenko@gmail.com>
2023-10-04 09:23:32 +0300

    add input parameters to QCM

--- project1/RandomGenerators.h
+++ project1/RandomGenerators.h
@@ -1,9 +1,8 @@
 
-
 #ifndef RandomGenerators_h
 #define RandomGenerators_h
 
-#pragma once 
+#pragma once
 
 #include <iostream>
 #include <math.h>
@@ -142,10 +141,10 @@ public:
 class GeneratorBase {
     
 public:
-    array<long long, 4> enterParametr(string Method){
-        array<long long, 4> parametrs = {};
+    long long * enterParametr(string Method, long long size){
+        long long *parametrs = new long long[size];
         if (Method == "LCM"){
-            long long m = 0, c = 0, a = 0, X0 = 0;
+            long long m = 0, c = 0, a = 0, X0 = 0, b;
             while (m == 0){
                 cout << "enter positive m: ";
                 cin >> m;
@@ -173,7 +172,7 @@ public:
                 
                 cout << "enter positive a: ";
                 cin >> a;
-                long long b = a - 1;
+                b = a - 1;
                 if (a < 0 && a >= m) {
                     a = 0;
                     continue;
@@ -182,9 +181,7 @@ public:
                 long long *arr = get<0>(primes);
                 long long n = get<1>(primes);
                 for (int i= 0 ; i <= n ; ++i) {
-//                    cout << b << " " << arr[i] << endl;
                     if (b % arr[i] != 0){
-//                        cout << b << " " << arr[i] << endl;
                         cout << "a-1 is not multiple to p " << endl;
                         a = 0;
                         break;
@@ -207,7 +204,99 @@ public:
                 parametrs[3] = X0;
             }
         }
-            
+        if (Method == "QCM"){
+            long m= 0 , a= 0, c=0, d=0, b=0;
+            while (m == 0){
+                cout << "enter positive m: ";
+                cin >> m;
+                if (m < 0) m = 0;
+               
+                
+            }
+            parametrs[0] = m;
+            while (c == 0){
+                cout << "enter positive c: ";
+                
+                cin >> c;
+                if (c < 0 && c >= m){
+                    c = 0;
+                    continue;
+                }
+                
+                if(gcd(m, c) != 1){
+                    cout << "m and c is not relatively prime" << endl;
+                    c = 0;
+                }
+            }
+            parametrs[1] = c;
+            while (a == 0){
+                
+                cout << "enter positive a: ";
+                cin >> a;
+                b = a - 1;
+                if (a < 0 && a >= m) {
+                    a = 0;
+                    continue;
+                };
+                tuple<long long *, long long> primes = generatePrimes(m, m);
+                long long *arr = get<0>(primes);
+                long long n = get<1>(primes);
+                for (int i= 0 ; i <= n ; ++i) {
+                    if (b % arr[i] != 0){
+                        cout << "a-1 is not multiple to p " << endl;
+                        a = 0;
+                        break;
+                    }
+                }
+            }
+            parametrs[2] = a;
+            while (d == 0){
+                cout << "enter positive m: ";
+                cin >> d;
+                if (d < 0){
+                    d = 0;
+                    continue;
+                    
+                };
+                if (d < 0 && d >= m) {
+                    d = 0;
+                    continue;
+                };
+                tuple<long long *, long long> primes = generatePrimes(m, m);
+                long long *arr = get<0>(primes);
+                long long n = get<1>(primes);
+                for (int i= 0 ; i <= n ; ++i) {
+                    if (d % arr[i] != 0){
+                        cout << "d is not multiple to p " << endl;
+                        d = 0;
+                        break;
+                    }
+                }
+                if(d == 0){
+                    continue;
+                }
+                if(m % 4 == 0){
+                    if(d % 2 != 0 || d != b%4){
+                        cout << " число d є парним і d = a–1 mod 4, якщо число m є кратним 4; " << endl;
+                        d = 0;
+                        continue;
+                    }
+                }
+                else if(m % 2 == 0){
+                    if(d != b%2){
+                        cout << " число d = a–1 mod 2 , якщо число m є кратним 2; " << endl;
+                        d = 0;
+                        continue;
+                    }
+                }
+                else if(m%3==0){
+                    if(d == (3*c)%9){
+                        cout << "d != 3c mod 9, якщо число m є кратним 3" << endl;
+                        d = 0;
+                    }
+                }
+            }
+        }
         return parametrs;
     }
     tuple<long long *, long long> generatePrimes(long long m, long long size) {
@@ -243,6 +332,10 @@ public:
                 
                 Primes = newPrimes;
             }
+        for(int i=0;i<PrimeIndex+1; ++i ){
+            cout << Primes[i] << ", ";
+        }
+        cout << endl;
         return make_tuple(Primes, PrimeIndex);
         
     }
@@ -256,8 +349,6 @@ public:
         return a;
     }
     long long LCM(long long m, long long X0, long long a, long long c){
-        
-        
         return (a*X0 +c) % m;
     }
     long long QCM(long long m, long long X0,  int d, int a, int c){
@@ -353,7 +444,7 @@ public:
     void linearCongruentialMethod() {
         long long X0, X1, a, c, m;
         long long* parametr[4] {&m, &c, &a, &X0};
-        array<long long, 4> enteredParametrs = enterParametr("LCM");
+        long long *enteredParametrs = enterParametr("LCM", 4);
         for(int i = 0; i < 4; i++){
             *parametr[i] = enteredParametrs[i];
         }
@@ -443,32 +534,41 @@ public:
 
 
 class UnionMethod: public GeneratorBase{
-private:
-    const long long m_;
 public:
-    UnionMethod(long long m)
-    : m_(m) {
+    UnionMethod(){
         
     }
     
     void unionMethod() {
-        long long X0 = 6, Y0 = 1, X1, Y1, Z ;
+        long long X0, Y0, X1, Y1, Z, m ;
         NumberVector unionMethod_vector;
-        unionMethod_vector.pushHundredEvenlyValue(X0-Y0, m_);
-        for (int i = 1; i < m_; ++i) {
-            X1 = LCM(m_, X0, 4, 6);
+        long long a1, c1, m1;
+        long long a2, c2, m2;
+        
+        cout << "enter m: ";
+        cin >> m;
+        long long* parametr1[4] {&m1, &c1, &a1, &X0};
+        long long *enteredParametrs1 = enterParametr("LCM", 4);
+        long long* parametr2[4] {&m2, &c2, &a2, &Y0};
+        long long *enteredParametrs2 = enterParametr("LCM", 4);
+        for(int i = 0; i < 4; i++){
+            *parametr1[i] = enteredParametrs1[i];
+            *parametr2[i] = enteredParametrs2[i];
+        }
+        for (int i = 0; i < m; ++i) {
+            X1 = LCM(m1, X0, a1, c1);
             X0 = X1;
-            Y1 = LCM(m_/1000, Y0, 6, 7);
+            Y1 = LCM(m2, Y0, a2, c2);
             Y0 = Y1;
             Z = (X1 - Y1);
             if(Z<0)
-                Z+=m_;
+                Z+=m;
             
-            Z%=m_;
+            Z%=m;
             
-            unionMethod_vector.pushHundredEvenlyValue(Z, m_);
+            unionMethod_vector.pushHundredEvenlyValue(Z, m);
         }
-        vector<Interval> vals = calcFrequency(unionMethod_vector, m_, IntervalEdges(0, 100, 10, true, true));
+        vector<Interval> vals = calcFrequency(unionMethod_vector, m, IntervalEdges(0, 100, 10, true, true));
         printResult(vector<vector<Interval>>{vals});
     }
 };
@@ -477,24 +577,29 @@ public:
 
 
 class SigmaMethod: public GeneratorBase{
-private:
-    long long m_;
-    
 public:
-    SigmaMethod(long long m)
-    : m_(m){}
+    SigmaMethod(){}
     
     void sigmaMethod() {
         double X0;
-        long long Y0 = 1, Y1;
+        long long Y0 = 1, Y1, m;
         NumberVector sigmaMethod_vector;
         array<double, 12> selected_numbers;
         double sum=0;
-        for (int i = 0; i < m_; ++i) {
+        long long a1, c1, m1;
+        
+        cout << "enter m: ";
+        cin >> m;
+        long long* parametr1[4] {&m1, &c1, &a1, &Y0};
+        long long *enteredParametrs1 = enterParametr("LCM", 4);
+        for(int i = 0; i < 4; i++){
+            *parametr1[i] = enteredParametrs1[i];
+        }
+        for (int i = 0; i < m; ++i) {
             for(int i = 0; i < 12; ++i){
-                Y1 = LCM(m_, Y0, 6, 7);
+                Y1 = LCM(m, Y0, a1, c1);
                 Y0 = Y1;
-                selected_numbers[i] = static_cast<double>(Y1)/static_cast<double>(m_-1);
+                selected_numbers[i] = static_cast<double>(Y1)/static_cast<double>(m-1);
                 sum += selected_numbers[i];
             }
             
@@ -504,30 +609,40 @@ public:
             
         }
         
-        vector<Interval> vals = calcFrequency(sigmaMethod_vector, m_, IntervalEdges(-3, 3, 12, true, true));
+        vector<Interval> vals = calcFrequency(sigmaMethod_vector, m, IntervalEdges(-3, 3, 12, true, true));
         vector<Interval> sub_interval = makeSubInterval(vals, 0, 1);
         printResult(vector<vector<Interval>>{vals, sub_interval});
     }
 };
 class PolarMethod: public GeneratorBase{
-private:
-    long long m_;
-    
+  
 public:
-    PolarMethod(long long m)
-    : m_(m){}
+    PolarMethod(){}
     void polarMehod(){
-        long long Y0 = 1, Y1 = 2, Y2, Y3, numSkipped= 0 ;
+        long long Y0 = 1, Y1 = 2, Y2, Y3, numSkipped= 0, m ;
         double S,U1 ,U2, V1, V2, X1, X2;
         NumberVector polarMethod_vector;
-        for(int i = 0; i < m_/2 + numSkipped; ++i){
+        long long a1, c1, m1;
+        long long a2, c2, m2;
+        
+        cout << "enter m: ";
+        cin >> m;
+        long long* parametr1[4] {&m1, &c1, &a1, &Y0};
+        long long *enteredParametrs1 = enterParametr("LCM", 4);
+        long long* parametr2[4] {&m2, &c2, &a2, &Y1};
+        long long *enteredParametrs2 = enterParametr("LCM", 4);
+        for(int i = 0; i < 4; i++){
+            *parametr1[i] = enteredParametrs1[i];
+            *parametr2[i] = enteredParametrs2[i];
+        }
+        for(int i = 0; i < m/2 + numSkipped; ++i){
             
-            Y2 = LCM(m_, Y0, 6, 7);
-            Y3 = LCM(m_, Y1, 2, 3);
+            Y2 = LCM(m1, Y0, a1, c1);
+            Y3 = LCM(m2, Y1, c2, a2);
             Y0 = Y2;
             Y1 = Y3;
-            U1 = static_cast<double>(Y2)/static_cast<double>(m_);
-            U2 = static_cast<double>(Y3)/static_cast<double>(m_);
+            U1 = static_cast<double>(Y2)/static_cast<double>(m);
+            U2 = static_cast<double>(Y3)/static_cast<double>(m);
             V1 = 2*U1-1;
             V2 = 2*U2-1;
             S = V1*V1 + V2*V2;
@@ -542,7 +657,7 @@ public:
             polarMethod_vector.insert(polarMethod_vector.returnVectorSize(), {X1, X2});
             
         }
-        vector<Interval> vals = calcFrequency(polarMethod_vector, m_, IntervalEdges(-3, 3, 12, true, true));
+        vector<Interval> vals = calcFrequency(polarMethod_vector, m, IntervalEdges(-3, 3, 12, true, true));
         vector <Interval> sub_interval = makeSubInterval(vals, 0, 1);
         printResult(vector<vector<Interval>>{vals, sub_interval});
         
@@ -550,29 +665,43 @@ public:
 };
 class RelationMethod: public GeneratorBase{
 private:
-    long long m_;
     
 public:
-    RelationMethod(long long m)
-    : m_(m){}
+    RelationMethod(){}
     void relationMethod(){
-        long long Y0 = 1, Y1 = 2, Y2, Y3, numSkipped= 0;
+        long long Y0, Y1, Y2, Y3, numSkipped= 0, m;
+        long long a1, c1, m1;
+        long long a2, c2, m2;
         double U1, V1, X;
+        
+        
+        
         NumberVector relationMethod_vector;
-        for(int i = 0; i < m_+numSkipped; ++i){
+        
+        cout << "enter m: ";
+        cin >> m;
+        long long* parametr1[4] {&m1, &c1, &a1, &Y0};
+        long long *enteredParametrs1 = enterParametr("LCM", 4);
+        long long* parametr2[4] {&m2, &c2, &a2, &Y1};
+        long long *enteredParametrs2 = enterParametr("LCM", 4);
+        for(int i = 0; i < 4; i++){
+            *parametr1[i] = enteredParametrs1[i];
+            *parametr2[i] = enteredParametrs2[i];
+        }
+        for(int i = 0; i < m+numSkipped; ++i){
             
-            Y2 = LCM(m_, Y0, 6, 7);
-            Y3 = LCM(m_, Y1, 2, 3);
+            Y2 = LCM(m1, Y0, a1, c1);
+            Y3 = LCM(m2, Y1, a2, c2);
             Y0 = Y2;
             Y1 = Y3;
-            U1 = static_cast<double>(Y2)/static_cast<double>(m_);
-            V1 = static_cast<double>(Y3)/static_cast<double>(m_);
+            U1 = static_cast<double>(Y2)/static_cast<double>(m);
+            V1 = static_cast<double>(Y3)/static_cast<double>(m);
             if(U1 == 0) {
                 numSkipped+=1;
                 continue;
                 
             }
-            V1 = static_cast<double>(Y3)/static_cast<double>(m_);
+            V1 = static_cast<double>(Y3)/static_cast<double>(m);
             X = sqrt(8.0/exp(1))*((V1-0.5)/U1);
             
             if(X*X <= (5-4*exp(0.25)*U1)){
@@ -590,7 +719,7 @@ public:
             }
             
         }
-        vector<Interval> vals = calcFrequency(relationMethod_vector, m_, IntervalEdges(-3, 3, 12, true, true));
+        vector<Interval> vals = calcFrequency(relationMethod_vector, m, IntervalEdges(-3, 3, 12, true, true));
         vector <Interval> sub_interval = makeSubInterval(vals, 0, 1);
         printResult(vector<vector<Interval>>{vals, sub_interval});
     }
@@ -600,26 +729,33 @@ public:
 
 class LogarithmMethod: public GeneratorBase {
 private:
-    const long long m_;
     
     
 public:
-    LogarithmMethod(long long m)
-    : m_(m){}
+    LogarithmMethod(){}
     
     void logarithmMethod() {
-        long long Y0=1, Y1;
+        long long Y0=1, Y1, m;
         double U0, X;
+        
         NumberVector logarithmMethod_vector;
         
-        for (int i = 1; i < m_; ++i) {
-            Y1 = LCM(m_, Y0, 2, 3);
+        long long a1, c1, m1;
+        cout << "enter m: ";
+        cin >> m;
+        long long* parametr1[4] {&m1, &c1, &a1, &Y0};
+        long long *enteredParametrs1 = enterParametr("LCM", 4);
+        for(int i = 0; i < 4; i++){
+            *parametr1[i] = enteredParametrs1[i];
+        }
+        for (int i = 1; i < m; ++i) {
+            Y1 = LCM(m1, Y0, a1, c1);
             Y0 = Y1;
-            U0 = static_cast<double>(Y1)/static_cast<double>(m_);
+            U0 = static_cast<double>(Y1)/static_cast<double>(m);
             X = -14*log(U0);
             logarithmMethod_vector.pushValue(X);
         }
-        vector<Interval> vals = calcFrequency(logarithmMethod_vector, m_, IntervalEdges(0, 100, 10, true, true));
+        vector<Interval> vals = calcFrequency(logarithmMethod_vector, m, IntervalEdges(0, 100, 10, true, true));
         printResult(vector<vector<Interval>>{vals});
     }
 };
@@ -666,4 +802,4 @@ public:
 
 
 
-#endif 
+#endif
